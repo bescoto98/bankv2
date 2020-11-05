557f608a80ce9f463b9611e80a7a2a78df5ca24e
bescoto98 <escoto.brenda42@gmail.com>
2020-11-05 11:21:19 -0800

    more register component work

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 
+import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [HomeComponent, RegisterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class LandingModule { }
--- src/app/landing/register/register.component.ts
+++ src/app/landing/register/register.component.ts
@@ -14,27 +14,37 @@ export class RegisterComponent implements OnInit {
   // placeholderText: string = "Enter...";
   index:number = 1;
   newestUser: NewUser;
-  createdUser: User;
-  newAccount: NewAccount;
+
+  selectedRole;
+  roles = [
+    "EMPLOYEE",
+    "CUSTOMER"
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.newestUser = new NewUser(
+      null, null, null, null,
+      null, null, null, null,
+      null, null, null, null
+    );
+    // this.newestUser.role = "CUSTOMER";
+    // console.log(this.newestUser);
   }
 
   increaseIndex(){
     this.index++;
   }
-  decreaseIndex(){
 
-    if(this.index >= 2){
-        this.index--;
-    }
-
+  setRole(){
+    // console.log(this.selectedRole)
+    this.newestUser.role = this.selectedRole;
   }
 
   makeUser(){
 
+    console.log(this.newestUser);
   }
 
 }
