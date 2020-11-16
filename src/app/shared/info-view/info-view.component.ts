df00de22f71849fd4834221437a2dd2e428693a3
bescoto98 <escoto.brenda42@gmail.com>
2020-11-16 11:51:19 -0800

    added personal info view component

--- src/app/shared/info-view/info-view.component.ts
+++ src/app/shared/info-view/info-view.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoViewComponent implements OnInit {
 
+  personalReadOnly: boolean = true;
+  addressReadOnly: boolean = true;
+  testInfo =
+  {
+    "infoid": 6,
+    "ssn": "***-**-5050",
+    "address": "308 Bristol St.",
+    "city": "Phoenix",
+    "state": "AZ",
+    "zip": "93447",
+    "phone": "4745652222",
+    "email": "riggs@gmail.com",
+    "owner": {
+        "userid": 105,
+        "firstname": "Bradley",
+        "lastname": "Riggs",
+        "username": "rigg93",
+        "password": "password",
+        "role": "EMPLOYEE"
+    }
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  editPersonal() {
+    this.personalReadOnly = false;
+  }
+
+  savePersonal() {
+    this.personalReadOnly = true;
+  }
+
+  editAddress() {
+    this.addressReadOnly = false;
+  }
+
+  saveAddress() {
+    this.addressReadOnly = true;
   }
 
 }
