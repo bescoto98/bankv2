e42c8ff461189363ad8cf54989ef8a7d17889868
bescoto98 <escoto.brenda42@gmail.com>
2020-11-17 13:15:31 -0800

    working on customer transaction page

--- src/app/customer/customer-transaction/customer-transaction.component.ts
+++ src/app/customer/customer-transaction/customer-transaction.component.ts
@@ -11,6 +11,46 @@ export class CustomerTransactionComponent implements OnInit {
   isDeposit: boolean = false;
   isTransfer: boolean = false;
 
+  testAccounts =
+  [
+    {
+        "accountid": 107,
+        "balance": 500.0,
+        "type": "CHECKING",
+        "status": "OPEN",
+        "approvedby": "Eagan Short",
+        "createdon": "10-15-2015",
+        "owner": {
+            "userid": 107,
+            "firstname": "Clinton",
+            "lastname": "Hughes",
+            "username": "hugh59",
+            "password": "password",
+            "role": "CUSTOMER"
+        }
+    },
+    {
+        "accountid": 108,
+        "balance": 700.0,
+        "type": "SAVINGS",
+        "status": "OPEN",
+        "approvedby": "Jerome Holder",
+        "createdon": "08-15-2012",
+        "owner": {
+            "userid": 107,
+            "firstname": "Clinton",
+            "lastname": "Hughes",
+            "username": "hugh59",
+            "password": "password",
+            "role": "CUSTOMER"
+        }
+    }
+  ];
+
+  accountSelected;
+  toAccount;
+  amount;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -35,6 +75,10 @@ export class CustomerTransactionComponent implements OnInit {
         this.isTransfer = true;
         break;
     }
+  }
+
+  selectAccount(){
+    console.log(this.accountSelected);
   }
 
 }
