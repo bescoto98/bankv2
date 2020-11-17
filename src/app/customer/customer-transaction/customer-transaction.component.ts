82f94beab52ba6c2f3684611df9e85978d7d8512
bescoto98 <escoto.brenda42@gmail.com>
2020-11-17 11:43:46 -0800

    adding page for money transactions

--- src/app/customer/customer-transaction/customer-transaction.component.ts
+++ src/app/customer/customer-transaction/customer-transaction.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerTransactionComponent implements OnInit {
 
+  isWithdraw: boolean = true;
+  isDeposit: boolean = false;
+  isTransfer: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeTab(changeTo: any) {
+    switch(changeTo)
+    {
+      case 'w':
+        this.isWithdraw = true;
+        this.isDeposit = false;
+        this.isTransfer = false;
+        break;
+      case 'd':
+        this.isWithdraw = false;
+        this.isDeposit = true;
+        this.isTransfer = false;
+        break;
+      case 't':
+        this.isWithdraw = false;
+        this.isDeposit = false;
+        this.isTransfer = true;
+        break;
+    }
   }
 
 }
